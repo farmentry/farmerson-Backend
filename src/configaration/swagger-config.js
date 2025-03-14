d657ca501e6c3fef2a991fa7a262f9f5fe5f4604
JeevanK-Work <jkottam@farmerson.net>
2025-03-14 20:29:23 +0530

    updated env file

--- src/configaration/swagger-config.js
+++ src/configaration/swagger-config.js
@@ -10,8 +10,9 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: `http://localhost:${process.env.PORT || 5000}`,
-      description: "Local server",
+      //   url: `http://localhost:${process.env.PORT || 5000}`,
+      url: `http://34.47.252.131`,
+      description: "UAT server",
     },
   ],
 };
